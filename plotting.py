d2afeb99045fd261800b609cf4da80f48a2793ab
Omar M Hussein <omagdy.222@gmail.com>
2021-04-02 15:17:30 +0200

    Show error in the plotting of generated images

--- plotting.py
+++ plotting.py
@@ -6,11 +6,12 @@ from loss_functions import psnr_and_ssim_loss
 def plot_random_slice(model_patch, r1, r2, PATCH_SIZE):
     model_patch = model_patch.reshape(PATCH_SIZE,PATCH_SIZE,PATCH_SIZE)
     if r1 == 1:
-        plt.imshow(model_patch[r2,:,:])
+        patch_slice = model_patch[r2,:,:]
     elif r1 == 2:
-        plt.imshow(model_patch[:,r2,:])
+        patch_slice = model_patch[:,r2,:]
     else:
-        plt.imshow(model_patch[:,:,r2])
+        patch_slice = model_patch[:,:,r2]
+    plt.imshow(patch_slice, cmap="gray")
     plt.axis('off')
     
     
@@ -26,9 +27,10 @@ def generate_images(prediction, test_input, tar, PATCH_SIZE, figure_name, epoch=
         ax = plt.subplot(1, 3, i+1)
         plt.title(title[i])
         psnr,ssim = psnr_and_ssim_loss(display_list[i],tar,PATCH_SIZE)
+        error = round(supervised_loss(tar, display_list[i]).numpy(), 3)
 #         text = 'PSNR={}\nSSIM={}'.format(psnr,ssim)
-        text = 'PSNR='+str(psnr)+'\nSSIM='+str(ssim)
-        plt.text(0.5,-0.2, text, size=20, ha="center", 
+        text = 'Error='+str(error)+'\nPSNR='+str(psnr)+'\nSSIM='+str(ssim)
+        plt.text(0.5,-0.25, text, size=20, ha="center", 
          transform=ax.transAxes)
         plot_random_slice(display_list[i], r1, r2, PATCH_SIZE)
     # plt.show()
@@ -39,8 +41,8 @@ def generate_images(prediction, test_input, tar, PATCH_SIZE, figure_name, epoch=
 def plot_evaluations(epochs_plot, training_psnr, validation_psnr, training_ssim, validation_ssim,
                      training_generator_g_error, validation_generator_g_error, EPOCH_START=""):
     plt.figure(num=None, figsize=(20, 15), dpi=80, facecolor='w', edgecolor='k')
-    plt.plot(epochs_plot, training_generator_g_error, label ='Training Genreator G Error')
-    plt.plot(epochs_plot, validation_generator_g_error, label ='Validation Genreator G Error')
+    plt.plot(epochs_plot, training_generator_g_error, label ='Training Error')
+    plt.plot(epochs_plot, validation_generator_g_error, label ='Validation Error')
     plt.legend()
     # plt.show()
     plt.savefig('generated_plots/losses_plot_{}.png'.format(EPOCH_START), bbox_inches='tight')
@@ -52,7 +54,7 @@ def plot_evaluations(epochs_plot, training_psnr, validation_psnr, training_ssim,
     plt.savefig('generated_plots/psnr_evaluation_plot_{}.png'.format(EPOCH_START), bbox_inches='tight')    
     plt.figure(num=None, figsize=(20, 15), dpi=80, facecolor='w', edgecolor='k')
     plt.plot(epochs_plot, training_ssim, label ='Training SSIM')
-    plt.plot(epochs_plot, validation_ssim, label ='Training SSIM')
+    plt.plot(epochs_plot, validation_ssim, label ='Validation SSIM')
     plt.legend()
     # plt.show()
     plt.savefig('generated_plots/ssim_evaluation_plot_{}.png'.format(EPOCH_START), bbox_inches='tight')  
