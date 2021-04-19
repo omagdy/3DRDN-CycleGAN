fbfc807514bc635248fc0f6e1c88cb4a3bc3e9c0
Omar M Hussein <omagdy.222@gmail.com>
2021-04-19 07:57:30 +0200

    Modify default arguement parameters

--- arg_parser.py
+++ arg_parser.py
@@ -6,7 +6,7 @@ def training_parser():
     parser.add_argument('-lr', '--learning_rate', action='store',
                          default=1e-4, type=float, help=('Learning Rate. Default: 0.0001'))
     parser.add_argument('-bs', '--batch_size', action='store', 
-                         default=6, type=int, help='Batch Size. Default: "6"')
+                         default=8, type=int, help='Batch Size. Default: "8"')
     parser.add_argument('-ep', '--epochs', action='store', default=1, 
                          type=int, help=('Epochs. Default: 1'))
     parser.add_argument('-eps', '--epoch_start', action='store', default=0, 
@@ -18,10 +18,10 @@ def training_parser():
                          type=int, help=('Growth rate of the filters for each layer in the dense block. Default: 12'))
     parser.add_argument('-ub', '--utilize_bias', action='store', default=True, 
                          type=bool, help=('Whether to utilize bias in the convolutional layers or not. Default: True'))
-    parser.add_argument('-ndb', '--no_of_dense_blocks', action='store', default=10, 
-                         type=int, help=('Number of dense blocks in the generator. Default: 10'))
-    parser.add_argument('-nub', '--no_of_units_per_block', action='store', default=4, 
-                         type=int, help=('Number of BNN-ELU-Conv layers in each dense block. Default: 4'))
+    parser.add_argument('-ndb', '--no_of_dense_blocks', action='store', default=2, 
+                         type=int, help=('Number of dense blocks in the generator. Default: 2'))
+    parser.add_argument('-nub', '--no_of_units_per_block', action='store', default=3, 
+                         type=int, help=('Number of BNN-LEAKYRELU-Conv layers in each dense block. Default: 3'))
     parser.add_argument('-wi', '--weights_init', action='store', default='HeUniform', 
                          type=str, choices=['HeUniform', 'HeNormal', 'GlorotUniform', 'GlorotNormal'],
                          help=('Initializer for the kernel weights matrix. Default: HeUniform'))
